52913304c814c2bb337bf502106af0bb7bfcea50
Yohan Jarosz <yohanjarosz@yahoo.fr>
2012-05-01 09:38:31 +0200

    can put different list on a form

--- joblauncher/controllers/form.py
+++ joblauncher/controllers/form.py
@@ -27,22 +27,21 @@ def parse_parameters(id, fields, **kw):
     childs_args are the ones to get the list of files.
     pp are the private parameters
     """
-    print "parse_parameters(%s, %s, %s)" % (id, fields, kw)
     value = {}
     child_args = {}
     pp = {}
-
+    save = {}
     for field in fields:
         if field.id is not None:
-            print field.id
             key = field.__class__.__name__
             if to_treat_as_file_list.has_key(key):
                 _list = json.loads(kw.get(field.id, "[]"))
-                child_args[field.id] = {'options' : dict([(_list[i], i) for i in xrange(len(list(_list)))])}
+                child_args[field.id] = {'options' : dict([(_list[i], _list[i]) for i in xrange(len(list(_list)))])}
+                save[field.id] = _list
             else :
                 value[field.id]=kw.get(field.id, None)
-
-    pp['id']=id
+    pp['id'] = id
+    pp['save_list'] = save
     return value, child_args, pp
 
 class FormController(BaseController):
@@ -55,9 +54,9 @@ class FormController(BaseController):
         """
         Method to get the operations list
         """
-        paths = plugin.get_plugins_path()
         operations_path = 'operations_path = %s' % json.dumps(plugin.get_plugins_path(), default=plugin.encode_tree)
         return {'page' : 'form', 'paths' : operations_path}
+        paths = plugin.get_plugins_path()
 
     @expose()
     def error(self, *args, **kw):
@@ -81,14 +80,9 @@ class FormController(BaseController):
         obj = plug.plugin_object
 
         form =  obj.output()
-
         value, child_args, _pp = parse_parameters(id, form.fields, **kw)
-
-
         tmpl_context.form = form(action=url('/form/launch'))
-
-        print child_args
-        value['_pp'] = _pp
+        value['_pp'] = json.dumps(_pp)
         return {'page' : 'form', 'title' : obj.title(), 'value' : value, 'ca' : child_args}
 
     @expose()
@@ -106,7 +100,7 @@ class FormController(BaseController):
         return self.launch(*args, **kw)
 
     @expose()
-    def launch(self, _pp, *args, **kw):
+    def launch(self, _pp, **kw):
         """
         Launch the tasks
         """
@@ -125,12 +119,14 @@ class FormController(BaseController):
             import traceback, sys
             etype, value, tb = sys.exc_info()
             traceback.print_exception(etype, value, tb)
+            for k, v in pp['save_list'].iteritems():
+                kw[k] = json.dumps(v)
+
             if kw.has_key('_pp'): del kw['_pp']
             flash(e, 'error')
             raise redirect(url('./index', params={'id' : form_id}, **kw))
 
-        task_id = tasks.plugin_process.delay(form_id, *args, **kw)
-        #tasks.plugin_process.delay(**kw)
+        task_id = tasks.plugin_process.delay(form_id, **kw)
 
         flash('Job launched')
         raise redirect(url('./done',  {'task_id' : task_id}))
