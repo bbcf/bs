869b366d2d78dd6ca1fc5ee33622448358b8d9d5
Yohan Jarosz <yohanjarosz@yahoo.fr>
2012-08-07 14:57:10 +0200

    name project

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -39,7 +39,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'bs'
+project = u'BioScript'
 copyright = u'2012, BBCF'
 
 # The version info for the project you're documenting, acts as replacement for
